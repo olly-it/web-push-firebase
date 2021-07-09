91280139951417f568ff7c07bde2882115cde859
Alessio Olivieri <Alessio.Olivieri@zodiakactive.com>
2021-07-09 14:49:17 +0200

    1st commit

---
+++ src/main/java/it/olly/webpushfirebase/WebPushFirebaseApplication.java
@@ -0,0 +1,13 @@
+package it.olly.webpushfirebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPushFirebaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebPushFirebaseApplication.class, args);
+	}
+
+}
